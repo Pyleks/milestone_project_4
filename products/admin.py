e95867253cfce51d43abfd7b808439dcd153862d
Pyleks <aleksander.andersen@outlook.ie>
2020-07-29 21:22:55 +0100

    Customized Admin Center

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Product, Category
+
 
 # Register your models here.
+# Lists the products by the categories below in admin center.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'brand',
+        'rating',
+        'image',
+
+    )
+
+    # This allow the products to be ordered by SKU, can be changed with any of the ones above.
+    ordering = ('sku',)
+
+
+# List the friendly name first and the name after.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+# Runs the classes above to display the edited admin view
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 
-# Create your models here.
+# Category class allow us to rename the Category in Admin to our choosing
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
