e2f904dcf12a98c08e77971704149063df73deb9
Pyleks <aleksander.andersen@outlook.ie>
2020-09-30 16:39:21 +0100

    Test deployment

--- milestone_project_4/settings.py
+++ milestone_project_4/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Environmental variable set doc: https://django-environ.readthedocs.io/en/latest/
+# Environmental: variable set doc: https://django-environ.readthedocs.io/en/latest/
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
