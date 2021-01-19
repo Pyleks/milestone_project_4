940d68d711ed05cbbe9b48c420692642513338dc
Pyleks <aleksander.andersen@outlook.ie>
2021-01-19 13:23:20 +0000

    Enabled allowed hosts for heroku

--- milestone_project_4/settings.py
+++ milestone_project_4/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['threads-milestone-project-4.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['threads-milestone-project-4.herokuapp.com', 'localhost']
 
 
 # Application definition
